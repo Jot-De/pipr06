d0680cd4a5aa5722cebe7cf399782948df345336
arozmary <01186988@pw.edu.pl>
2023-11-27 20:53:58 +0100

    final changes

--- receipts/client.py
+++ receipts/client.py
@@ -1,46 +1,82 @@
+from distutils.log import info
 from typing import List
-from .price import Price
-from .receipt import Receipt
-
+from price import Price
+from receipt import Receipt, generate_receipts, random_receipt
+import random
+ 
 class Client:
     def __init__(self, name, user_id, receipts: list[Receipt] = None):
         self._name = name
-        # Add more here
-
+        self._user_id = user_id
+        self._receipts = receipts
+ 
     @property
-    #this method is already done
+    # this method is already done
     def name(self):
         return self._name
-
+ 
     @property
     def user_id(self):
-        pass
-
+        return self._user_id
+ 
     @property
     def receipts(self):
-        pass
-
+        return self._receipts
+ 
     def add_receipt(self, receipt: Receipt):
-        pass
-
+        self._receipts.append(receipt)
+ 
     @property
     def total_spending(self):
-        # HINT try to use sum() built-in function here
+        total_spendings = 0
+        for element in self._receipts:
+            total_spendings += element.total_price()
+        return total_spendings
+ 
+        # total_spending = [element for element in self._receipts]
+        # return sum(total_spendings)
         # HINT you can also use a list comprehension
-        pass
-
+ 
+    def info_user(self):
+        name_ID = self._name
+        spends = self.total_spending()
+        return f"Client {name_ID} has spent {spends} zł."
+ 
     def __str__(self):
-        pass
-
+        return self.info_user()
+ 
+    def info_developer(self):
+        name_ID = self._name
+        ID = self._user_id
+        spends = self.total_spending()
+        return f"Name: {name_ID}.\n Id: {ID}.\n Spends: {spends}zł."
+ 
     def __repr__(self):
-        pass
-
-
+        return self.info_developer()
+ 
+ 
 def random_client() -> Client:
     """Generates a random user"""
-
-
-def generate_clients(
-    num_clients: int, with_receipts=False, num_receipts=10
-) -> List[Client]:
+    name_list = ["Mike", "Alex", "George", "Sally", "John", 'Max', 'Gregory']
+    random_name = random.choice(name_list)
+    random_id = random.randint(0, 100)
+    random_receip = random_receipt()
+    my_client = Client(random_name, random_id, random_receip)
+    return my_client
+ 
+ 
+def generate_clients(num_clients: int, with_receipts=False, num_receipts=10) -> List[Client]:
     """Generates a given number of fake clients with optional fake receipts"""
+    client_list = []
+    if bool(with_receipts):
+        receipts = generate_receipts(num_clients)
+        for number in range(0, num_clients):
+            my_client = random_client()
+            my_client.add_receipt(receipts[number])
+            client_list.append(my_client)
+    else:
+        for number in range(0, num_clients):
+            my_client = random_client()
+            client_list.append(my_client)
+    return client_list
+ 
